71a9fcb0dca4a0f0b87134f096aa8b8048956a49
rushi-m45 <rushimonpara454@gmail.com>
2024-07-02 22:03:51 +0530

    Add files via upload

---
+++ Encryption of Ceaser Cipher.py
@@ -0,0 +1,19 @@
+# Code For Encryption
+
+def encrypt(string, shift):
+
+  cipher = ''
+  for char in string: 
+    if char == ' ':
+      cipher = cipher + char
+    elif  char.isupper():
+      cipher = cipher + chr((ord(char) + shift - 65) % 26 + 65)
+    else:
+      cipher = cipher + chr((ord(char) + shift - 97) % 26 + 97)
+  
+  return cipher
+
+text = input("Enter Word: ")
+s = int(input("Enter Shift Number: "))
+print("Original String: ", text)
+print("After Encryption: ", encrypt(text, s))
